119f4e82849f288ffe55e19d5707babe891d6084
bahaa95 <probahaa3@gmail.com>
2023-04-11 21:24:55 +0300

    refactor: refactor group, staff pages

--- src/features/videos/pages/group/group.tsx
+++ src/features/videos/pages/group/group.tsx
@@ -1,5 +1,16 @@
 import { useParams } from 'react-router-dom';
-import { Box, Helmet, Main, MainLayout, Section, Stack, StackItem, Text } from 'src/components';
+import {
+  Box,
+  Helmet,
+  Main,
+  MainLayout,
+  Section,
+  Stack,
+  StackItem,
+  Text,
+  Loader,
+  NoContent,
+} from 'src/components';
 import { useGetGroup } from '../../api';
 import { VideoList } from '../../components';
 import styles from './Group.module.scss';
@@ -8,29 +19,38 @@ export const Group = () => {
   const params = useParams<{ _id: string }>();
   const groupQuery = useGetGroup({ groupId: params._id as string });
 
-  if (groupQuery.isLoading) {
-    return <h1>Loading...</h1>;
+  if (groupQuery.isError) {
+    return (
+      <MainLayout>
+        <Helmet title="no content" />
+        <NoContent />
+      </MainLayout>
+    );
   }
 
   return (
     <MainLayout>
       <Helmet title={`${groupQuery.data?.title || 'group'}`} />
-      <Main className={`${styles.group}`}>
-        <Section>
-          <Box>
-            <Stack>
-              <StackItem>
-                <Text className={`text-white capitalize ${styles.title}`}>
-                  {groupQuery.data?.title}
-                </Text>
-              </StackItem>
-              <StackItem>
-                <VideoList videos={groupQuery.data?.videos || []} />
-              </StackItem>
-            </Stack>
-          </Box>
-        </Section>
-      </Main>
+      {groupQuery.isLoading ? (
+        <Loader />
+      ) : (
+        <Main className={`${styles.group}`}>
+          <Section>
+            <Box>
+              <Stack>
+                <StackItem>
+                  <Text className={`text-white capitalize ${styles.title}`}>
+                    {groupQuery.data?.title}
+                  </Text>
+                </StackItem>
+                <StackItem>
+                  <VideoList videos={groupQuery.data?.videos || []} />
+                </StackItem>
+              </Stack>
+            </Box>
+          </Section>
+        </Main>
+      )}
     </MainLayout>
   );
 };
--- src/features/videos/pages/staff/staff.tsx
+++ src/features/videos/pages/staff/staff.tsx
@@ -1,5 +1,16 @@
 import { useParams } from 'react-router-dom';
-import { Helmet, Main, MainLayout, Section, Box, Stack, StackItem, Image } from 'src/components';
+import {
+  Helmet,
+  Main,
+  MainLayout,
+  Section,
+  Box,
+  Stack,
+  StackItem,
+  Image,
+  Loader,
+  NoContent,
+} from 'src/components';
 import { useGetPerson } from '../../api';
 import { VideoList } from '../../components/videoList';
 import styles from './Staff.module.scss';
@@ -7,41 +18,53 @@ import styles from './Staff.module.scss';
 export const Staff = () => {
   const params = useParams<{ _id: string }>();
   const { data: person, ...personQuery } = useGetPerson({ _id: params._id as string });
-  console.log(person);
-  if (personQuery.isLoading) {
-    return <h1>loading...</h1>;
+
+  if (personQuery.isError) {
+    return (
+      <MainLayout>
+        <Helmet title="no content" />
+        <NoContent />
+      </MainLayout>
+    );
   }
 
   return (
     <MainLayout>
       <Helmet title={person?.name || 'no content'} />
-      <Main className={`bg-dark-200 ${styles.staff}`}>
-        <Section className={`${styles.section}`}>
-          <Box>
-            <Stack direction={'row'} align={'center'}>
-              <StackItem>
-                <Box className={`${styles.imageContainer}`}>
-                  <Image
-                    className={`${styles.image}`}
-                    src={person?.image?.url}
-                    alt={person?.name}
-                  />
-                </Box>
-              </StackItem>
-              <StackItem>
-                <Box>
-                  <h2 className={`text-white capitalize ${styles.name}`}>{person?.name}</h2>
-                </Box>
-              </StackItem>
-            </Stack>
-          </Box>
-        </Section>
-        <Section className={`${styles.section}`}>
-          <Box>
-            <VideoList videos={person?.videos || []} />
-          </Box>
-        </Section>
-      </Main>
+      {personQuery.isLoading ? (
+        <Loader />
+      ) : (
+        <Main className={`bg-dark-200 ${styles.staff}`}>
+          <Section className={`${styles.section}`}>
+            <Box>
+              <Stack direction={'row'} align={'center'}>
+                {/* person image */}
+                <StackItem>
+                  <Box className={`${styles.imageContainer}`}>
+                    <Image
+                      className={`${styles.image}`}
+                      src={person?.image?.url}
+                      alt={person?.name}
+                    />
+                  </Box>
+                </StackItem>
+                {/* person name */}
+                <StackItem>
+                  <Box>
+                    <h2 className={`text-white capitalize ${styles.name}`}>{person?.name}</h2>
+                  </Box>
+                </StackItem>
+              </Stack>
+            </Box>
+          </Section>
+          <Section className={`${styles.section}`}>
+            <Box>
+              {/* person videos */}
+              <VideoList videos={person?.videos || []} />
+            </Box>
+          </Section>
+        </Main>
+      )}
     </MainLayout>
   );
 };

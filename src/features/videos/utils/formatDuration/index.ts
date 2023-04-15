e85dbb083170fed8ed5180e49e92d739352f1aa9
bahaa95 <probahaa3@gmail.com>
2023-04-15 16:08:38 +0300

    fix: fix time format when duration more than 3600

--- src/features/videos/utils/formatDuration/index.ts
+++ src/features/videos/utils/formatDuration/index.ts
@@ -1,17 +1,37 @@
 /**
- *
+ * @access public
  * @param duration Duration in seconds.
  * @returns string
  * @example
  * ```ts
- * formatDuration(9720); // 2:42:25
- * formatDuration(3500); // 58:20
+ * formatDuration(9720); // "2:42:25"
+ * formatDuration(3500); // "58:20"
  * ```
  */
 export function formatDuration(duration: number): string {
   const seconds = duration % 60;
   const minute = Math.trunc((duration / 60) % 60);
   const hours = Math.trunc(duration / 60 / 60);
-  const format = hours > 0 ? `${hours}:${minute}:${seconds}` : `${minute}:${seconds}`;
+
+  const formatHours = hours > 0 ? formatTime(hours) : null;
+  const formatMinute = formatTime(minute);
+  const formatSeconds = formatTime(seconds);
+
+  const format = formatHours
+    ? `${formatHours}:${formatMinute}:${formatSeconds}`
+    : `${formatMinute}:${formatSeconds}`;
+
   return format;
+}
+
+/**
+ * Add 0 if time less than 10
+ * @access privte
+ * @example
+ * ```ts
+ * formatTime(2); // => "02"
+ * ```
+ */
+function formatTime(time: number): string {
+  return time < 10 ? `0${time}` : `${time}`;
 }
